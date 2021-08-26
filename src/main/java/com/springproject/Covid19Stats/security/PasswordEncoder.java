a854eb0a5fdee6509524002a7c7c494c9ca7fdf0
MullaiArgent <mullairajan2000@gmail.com>
2021-08-26 12:35:34 +0530

    Security Layer

--- src/main/java/com/springproject/Covid19Stats/security/PasswordEncoder.java
+++ src/main/java/com/springproject/Covid19Stats/security/PasswordEncoder.java
@@ -1,4 +1,14 @@
 package com.springproject.Covid19Stats.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
 public class PasswordEncoder {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder (){
+            return new BCryptPasswordEncoder();
+            }
 }
--- src/main/java/com/springproject/Covid19Stats/security/config/WebSecurityConfig.java
+++ src/main/java/com/springproject/Covid19Stats/security/config/WebSecurityConfig.java
@@ -1,18 +1,43 @@
 package com.springproject.Covid19Stats.security.config;
 
+import com.springproject.Covid19Stats.appuser.AppUserService;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @AllArgsConstructor
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final AppUserService appUserService;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/api/v*/register/**")
+                .permitAll()
+                .anyRequest().authenticated().and()
+                .formLogin();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(daoAuthenticationProvider());
+
+    }
+    public DaoAuthenticationProvider daoAuthenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(bCryptPasswordEncoder);
+        provider.setUserDetailsService(appUserService);
+        return provider;
     }
 }
